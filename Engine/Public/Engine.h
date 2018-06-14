588d8dcff5bb91d0dd8b3dc2797ddd656cdbb77d
Alfredo Quintero Tlacuilo <alfredoq98@hotmail.com>
2018-06-13 21:23:45 -0700

    Created the engine abstract class to start putting the 3d stuff

--- Engine/Public/Engine.h
+++ Engine/Public/Engine.h
@@ -13,31 +13,29 @@ class Engine
 {
 public:
 
-	int Init(IGame* pGame, const char* pTitle, const int& pWidth, const int& pHeight);
+	virtual int Init(IGame* pGame, const char* pTitle, const int& pWidth, const int& pHeight) = 0;
 
 	// Run
-	void Run(IGame* pGame);
+	virtual void Run(IGame* pGame) = 0;
 
-	void Quit(IGame* pGame) const;
+	virtual void Quit(IGame* pGame) const = 0;
 
 	// TODO: Provide functionality for order in layer
-	void DrawLine(const Vector2& pOrigin, const Vector2& pTarget, const Color& pColor);
-	void DrawFillCircle(const Vector2& pPosition, const float& pRadius, const Color& pColor);
-	void DrawCircle(const Vector2& pPosition, const float& pRadius, const Color& pColor);
-	void DrawRect(const Vector2& pTopLeft, const Vector2& pBotRight, const Color& pColor);
+	virtual void DrawLine(const Vector2& pOrigin, const Vector2& pTarget, const Color& pColor) = 0;
+	virtual void DrawFillCircle(const Vector2& pPosition, const float& pRadius, const Color& pColor) = 0;
+	virtual void DrawCircle(const Vector2& pPosition, const float& pRadius, const Color& pColor) = 0;
+	virtual void DrawRect(const Vector2& pTopLeft, const Vector2& pBotRight, const Color& pColor) = 0;
 
 	// This will open and close the font everytime, so it may be a little slow, but I can set different sizes every time
 	// Drawn on top left
-	void DrawText(std::string pFontFile, const int& pFontSize, const std::string pMessage, const Vector2& pPosition, const Color& pColor);
+	virtual void DrawText(std::string pFontFile, const int& pFontSize, const std::string pMessage, const Vector2& pPosition, const Color& pColor) = 0;
 
-private:
+protected:
 
 	SDL_Window* mWindow;
 	SDL_Renderer* mRenderer;
 
 	int mWindowWidth;
 	int mWindowHeight;
-
-
 
 };
---
+++ Engine/Public/Engine2D.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "Engine.h"
+
+struct SDL_Window;
+struct SDL_Renderer;
+struct Color;
+struct Vector2;
+class IGame;
+
+// Defines the entry point for the engine
+class Engine2D : public Engine
+{
+public:
+
+	virtual int Init(IGame* pGame, const char* pTitle, const int& pWidth, const int& pHeight) override;
+
+	// Run
+	virtual void Run(IGame* pGame) override;
+
+	virtual void Quit(IGame* pGame) const override;
+
+	// TODO: Provide functionality for order in layer
+	virtual void DrawLine(const Vector2& pOrigin, const Vector2& pTarget, const Color& pColor) override;
+	virtual void DrawFillCircle(const Vector2& pPosition, const float& pRadius, const Color& pColor) override;
+	virtual void DrawCircle(const Vector2& pPosition, const float& pRadius, const Color& pColor) override;
+	virtual void DrawRect(const Vector2& pTopLeft, const Vector2& pBotRight, const Color& pColor) override;
+
+	// This will open and close the font everytime, so it may be a little slow, but I can set different sizes every time
+	// Drawn on top left
+	virtual void DrawText(std::string pFontFile, const int& pFontSize, const std::string pMessage, const Vector2& pPosition, const Color& pColor) override;
+
+protected:
+
+	SDL_Window * mWindow;
+	SDL_Renderer* mRenderer;
+
+	int mWindowWidth;
+	int mWindowHeight;
+
+};
